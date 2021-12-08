5f1c11edfd4631b4751fd33c7b7f8bddef08e49e
Frogkiller <frogkillerpl@gmail.com>
2021-12-08 16:13:44 +0100

    Added test token

--- base.sol
+++ base.sol
@@ -3,3 +3,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract token is ERC1155 {
+    uint256 public constant FUNGIBLE = 0;
+    uint256 public constant NON_FUNGIBLE = 1;
+
+    constructor() ERC1155("JSON_URI") {
+        _mint(msg.sender, FUNGIBLE, 100, "");
+        _mint(msg.sender, NON_FUNGIBLE, 1, "");
+    }
+}
