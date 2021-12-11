b3d6e7bf80540df9296d37eee02700bdd28ab1c5
Frogkiller <frogkillerpl@gmail.com>
2021-12-11 10:25:15 +0100

    Added mint function and getTokenDetails.
    Token is now Pausable, not sure how it's works tho.

--- base.sol
+++ base.sol
@@ -3,13 +3,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Pausable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract token is ERC1155 {
-    uint256 public constant FUNGIBLE = 0;
-    uint256 public constant NON_FUNGIBLE = 1;
+contract token is ERC1155, Ownable, Pausable {
+    uint256 public constant SCRAP = 0;
+    uint256 public constant FUEL = 1;
+
+    struct Ship {
+        uint8 damage;
+    }
+    uint256 nextId = 10;
+
+    mapping (uint256 => Ship) private _tokenDetails;
+
 
     constructor() ERC1155("JSON_URI") {
-        _mint(msg.sender, FUNGIBLE, 100, "");
-        _mint(msg.sender, NON_FUNGIBLE, 1, "");
+    }
+
+    function getTokenDetails(uint256 tokenId) public view returns(Ship memory){
+        return _tokenDetails[tokenId];
+    }
+
+    function mintShip(uint8 damage) public onlyOwner{
+        _tokenDetails[nextId] = Ship(damage);
+        _mint(msg.sender, nextId, 1, "");
+        nextId++;
     }
 }
